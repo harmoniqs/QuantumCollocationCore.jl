2ff9e6ddac903ed5440e807a23f3ad30d3c76c02
Jack Champagne <jchampag@andrew.cmu.edu>
2025-02-28 02:31:52 -0500

    fix canon. for pkg name

--- docs/make.jl
+++ docs/make.jl
@@ -24,7 +24,7 @@ pages = [
 
 format = Documenter.HTML(;
     prettyurls=get(ENV, "CI", "false") == "true",
-    canonical="https://docs.harmoniqs.co/",
+    canonical="https://docs.harmoniqs.co/QuantumCollocationCore.jl",
     edit_link="main",
     assets=String[],
     mathengine = MathJax3(Dict(
